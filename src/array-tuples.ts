8b3ab34f2b3a1accb41dcc8785356c3e2dd40025
unknown <md.asif.8393@gmail.com>
2023-05-24 13:15:36 +0600

    tuple

--- src/array-tuples.ts
+++ src/array-tuples.ts
@@ -1,2 +1,7 @@
 //array
-const heroes:string[] = ["Ananta", "Bappa", "Siam"]
+const heroes:string[] = ["Ananta", "Bappa", "Siam"];
+const nums: number[] = [23, 43, 53,234];
+const herInfo: (string | number | boolean)[] = ["khan", "ACtor", 32, true];
+
+//tuple
+const heroTuple: [string, number, boolean, string] = ["Sakib Khan", 34, true, "saaws"];
